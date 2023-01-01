bb2bf765b4cc80dcdbbc4a6d3671f3d74e35f992
Bogdan Radu <117690765+warl0cker@users.noreply.github.com>
2023-01-01 13:15:38 +0200

    Update 6.c

--- teme/tema_de_vacanta/vectori/6.c
+++ teme/tema_de_vacanta/vectori/6.c
@@ -1,8 +1,7 @@
 // 6. Verificati daca elementele unui vector de dimensiune n formeaza o progresie geometrica. Prototip: int progresie_geom(float v[], int n);
 int progresie_geom(float v[], int n) {
     int p = 1;
-    float r = v[2] / v[1];
     for (int i = 2; i < n-1; i++)
-        if (v[i + 1] / v[i] != r) p = 0;
-    return ((p == 1) ? 1 : 0);
+        if (v[i] / v[1] != v[i - 1]) p = 0;
+    return p;
 }
